aecf261e0a268d4a48b9f918e0757e7ddc8c9333
anhto2015 <minhboyx298@gmail.com>
2021-05-30 10:31:17 +0700

    ud

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -47,30 +47,32 @@ module.exports.index = async (req, res) => {
 
 module.exports.momo_notify = async (req, res) => {
     const data = req.body;
-    console.log(data);
-    const order = JSON.parse(data.extraData);
-    const date = moment().format('LL');
-    const orderTime = moment().format('LLL');
-    const newOrder = new Order({
-        userId: order.id,
-        userName: order.name,
-        email: order.email,
-        address: order.address,
-        city: order.city,
-        district: order.district,
-        phone: order.phone,
-        cart: order.cartItems,
-        payment: order.payment,
-        totalPrice: order.totalPrice,
-        date: date,
-        orderTime: orderTime,
-        status: 1,
-        orderId: order.orderId
-    })
-    try {
-        const savedOrder = await newOrder.save();
-        res.status(200).json('ok');
-    } catch (err) {
-        res.status(400).send(err);
+    console.log(data.errorCode);
+    if (data.errorCode ===  '0') {
+        const order = JSON.parse(data.extraData);
+        const date = moment().format('LL');
+        const orderTime = moment().format('LLL');
+        const newOrder = new Order({
+            userId: order.id,
+            userName: order.name,
+            email: order.email,
+            address: order.address,
+            city: order.city,
+            district: order.district,
+            phone: order.phone,
+            cart: order.cartItems,
+            payment: order.payment,
+            totalPrice: order.totalPrice,
+            date: date,
+            orderTime: orderTime,
+            status: 1,
+            orderId: order.orderId
+        })
+        try {
+            const savedOrder = await newOrder.save();
+            res.status(200).json('ok');
+        } catch (err) {
+            res.status(400).send(err);
+        }
     }
 }
